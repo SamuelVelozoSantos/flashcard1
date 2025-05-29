8a104a89a9aa2aa6019623f44da21bc1972d407c
SamuelVelozoSantos <velozo.santos@escola.pr.gov.br>
2025-05-29 10:53:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Sociologia',
+    'O que é Sociologia?',
+    'A ciência que estuda a sociedade'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'História',
+    'O que foi o Estopin da Primeira Guerra Mundial?',
+    'O assassinato do arquiduque Francisco Ferdinando'
+)
